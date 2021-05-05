ff844e7afdf8784a790a1ad64cc212a888bf7958
supert111 <sportsmen@bigmir.net>
2021-05-06 00:12:38 +0300

    settingsModal

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,11 @@
 import styles from './ImageGalleryItem.module.css';
 
-const ImageGalleryitem = ({ hits }) => {
+const ImageGalleryitem = ({ hits, onClick }) => {
+
     return (
         <>
-            { hits.map(({id, webformatURL, largeImageURL }) =>  ( 
-                <li key={id} className={styles.ImageGalleryItem}>
+            { hits.map(({ id, webformatURL }) =>  ( 
+                <li key={id} onClick={()=>onClick(id) } className={styles.ImageGalleryItem}>
                     <img id={id} src={webformatURL} alt="" className={styles.ImageGalleryItem_image} />
                 </li>
             )) }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,19 +2,38 @@ import { Component } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 
-const modalRoot = document.querySelector('#modal-root');
+ const modalRoot = document.querySelector('#modal-root');
 
 class Modal extends Component {
-    
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleKeyDown);
+    }
+
+    handleKeyDown = e => {
+        if(e.code === 'Escape') {
+            this.props.onClose();
+        }
+    }
+
+    handleBackdropClick = e => {
+        if(e.currentTarget === e.target) {
+            this.props.onClose();
+        }
+    }
+
     render() {
-        return createPortal(
-            <div className={styles.Overlay}>
+        return createPortal (
+            <div className={styles.Overlay} onClick={this.handleBackdropClick}>
                 <div className={styles.Modal}>
-                    <img src="" alt="" />
+                    { this.props.children }
                 </div>
             </div>,
-            modalRoot,
-        );
+             modalRoot,
+        )
     }
 
 }
